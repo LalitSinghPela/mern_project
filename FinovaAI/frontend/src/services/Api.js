e715bc3cc8e8a936c03541a154931d2bd0408fab
niten <dolaroast@gmail.com>
2026-05-20 15:09:11 +0530

    fixing signup issue

--- FinovaAI/frontend/src/services/Api.js
+++ FinovaAI/frontend/src/services/Api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-baseURL: import.meta.env.VITE_API_URL,
+baseURL: "https://finova-1a51.onrender.com/api",
  withCredentials: true
 
 });
